13a1ffa29c85159f54fc991043bd53383f50264e
maicallatrace <maicallatrace@gmail.com>
2022-05-25 22:36:35 -0600

    REPO

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+document.body.classList.toggle('light-theme');
+document.body.classList.toggle('dark-theme');
+const className = document.body.className;
+if(className == "light-theme") {
+this.textContent = "Dark";
+} else {
+this.textContent = "Light";
+}
+console.log('current class name: ' + className);
+});
+
+
+
 const likeBtn = document.getElementById("like-btn");
 
 window.addEventListener("load", () => {
